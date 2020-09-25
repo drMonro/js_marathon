31007fe651feae405ee9d1ba7e5196f594c21ee1
drMonro <Bestpower13>
2020-09-25 22:27:40 +0700

    HomeWork 3

--- main.js
+++ main.js
@@ -1,122 +1,81 @@
-let taskNumber = prompt('Какой номер задания? 1 или 2?', '2');
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    HP: 100,
+    elHPLabel: document.getElementById('health-character'),
+    elHPBar: document.getElementById('progressbar-character'),
+}
 
-// Задание #1
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    HP: 100,
+    elHPLabel: document.getElementById('health-enemy'),
+    elHPBar: document.getElementById('progressbar-enemy'),
+}
 
-let firstRow = 'мама мыла раму';
-let secondRow = 'собака друг человека';
-let letter = 'а';
-
-function getRow(firstRow, secondRow) {
-    let firstCounter = 0;
-    let secondCounter = 0;
-
-    for (let i = 0; i <= firstRow.length - 1; i++) {
-        if (firstRow.charAt(i) === letter) {
-            ++firstCounter;
-        }
+const actions = [
+    {
+        elButton: document.getElementById('btn-kick'),
+        damageMultiplier: 20
+    },
+    {
+        elButton: document.getElementById('btn-kick-spec'),
+        damageMultiplier: 30
     }
+]
 
-    for (let i = 0; i <= secondRow.length - 1; i++) {
-        if (secondRow.charAt(i) === letter) {
-            ++secondCounter;
-        }
-    }
 
-    if (firstCounter > secondCounter) {
-        return firstRow
-    } else if (firstCounter === secondCounter) {
-        return 'Колличество букв "' + letter + '" в строках равно'
+function renderHP(person) {
+    renderHPLife(person);
+    renderProgressBar(person);
+}
+
+function renderHPLife(person) {
+    person.elHPLabel.innerText = person.HP + '/' + person.defaultHP;
+}
+
+function renderProgressBar(person) {
+    person.elHPBar.style.width = person.HP + '%';
+}
+
+function changeHP(count, person) {
+    if (count > person.HP) {
+        person.HP = 0;
+        alert('Бедный ' + person.name + ' проиграл бой!');
+        disableALlButtons(actions);
     } else {
-        return secondRow
+        person.HP -= count;
+    }
+    renderHP(person);
+}
+
+function random(num) {
+    return Math.ceil(Math.random() * num);
+}
+
+function setupHitButtons(settings) {
+    for (let i = 0; i < settings.length; i++) {
+        settings[i].elButton.addEventListener('click', function () {
+            console.log('Kick');
+            changeHP(random(settings[i].damageMultiplier), character);
+            changeHP(random(settings[i].damageMultiplier), enemy);
+        });
     }
 }
 
-function askRowsAndLetter() {
-    firstRow = prompt('Введите строку #1', firstRow);
-    secondRow = prompt('Введите строку #2', secondRow);
-    letter = prompt('Введите букву для поиска', letter);
-}
-
-function runFirstTask() {
-    alert('Это программа для сравнения количества одинаковых букв в строках');
-    askRowsAndLetter();
-    console.log(getRow(firstRow, secondRow));
-}
-
-// Задание #2
-
-let phone = '+71234567890';
-// let phone = '89211234567';
-// let phone = '9211234567';
-
-function formatPhone(phone) {
-    let result = '';
-    console.log(phone.length)
-    if (phone.length === 12) {
-        for (let i = 0; i <= phone.length - 1; i++) {
-
-            if (i === 2) {
-                result += ' (' + phone.charAt(i);
-            } else if (i === 4) {
-                result += phone.charAt(i) + ') ';
-            } else if (i === 7 || i === 9) {
-                result += phone.charAt(i) + '-';
-            } else {
-                result += phone.charAt(i);
-            }
-        }
-        return result
-    } else if (phone.length === 11) {
-        for (let i = 0; i <= phone.length - 1; i++) {
-            if (i === 0) {
-                result += '+7 (';
-            } else if (i === 3) {
-                result += phone.charAt(i) + ') ';
-            } else if (i === 6 || i === 8) {
-                result += phone.charAt(i) + '-';
-            } else {
-                result += phone.charAt(i);
-            }
-        }
-        return result
-
-    } else if (phone.length === 10) {
-        for (let i = 0; i <= phone.length - 1; i++) {
-            if (i === 0) {
-                result += '+7 (' + phone.charAt(i);
-            } else if (i === 2) {
-                result += phone.charAt(i) + ') ';
-            } else if (i === 5 || i === 7) {
-                result += phone.charAt(i) + '-';
-            } else {
-                result += phone.charAt(i);
-            }
-        }
-        return result
-
-    } else {
-        alert('Похоже вы ввели не сотовый номер телефона');
-        runSecondTask()
-        return result
+function disableALlButtons(buttons) {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].elButton.disabled = true;
     }
-
 }
 
-function askPhone() {
-    phone = prompt('Введите телефон', phone);
-}
-
-function runSecondTask() {
-    alert('Введите сотовый номер телефона');
-    askPhone();
-    console.log(formatPhone(phone)); // +7 (123) 456-78-90
+function init() {
+    console.log('Start Game!');
+    renderHP(character);
+    renderHP(enemy);
+    setupHitButtons(actions);
 }
 
 
-
-if (taskNumber === "1") {
-    runFirstTask()
-} else if (taskNumber === "2") {
-    runSecondTask()
-}
-
+init();
