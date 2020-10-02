f18009167a8db838c0e0162597253b3a3eec7c53
drMonro <Bestpower13>
2020-10-02 23:52:57 +0700

    HomeWork 6

--- main.js
+++ main.js
@@ -25,12 +25,22 @@ const enemy = {
 
 const actions = [
     {
+        name: 'Айскик',
         elButton: document.getElementById('btn-kick'),
-        damageMultiplier: 20
+        damageMultiplier: 20,
+        counter: 0,
+        increaseCounter,
+        makeActionLog,
+        limit: 7
     },
     {
+        name: 'Фаербол',
         elButton: document.getElementById('btn-kick-spec'),
-        damageMultiplier: 30
+        damageMultiplier: 30,
+        counter: 0,
+        increaseCounter,
+        makeActionLog,
+        limit: 3
     }
 ]
 
@@ -45,62 +55,54 @@ function renderHPLabel() {
 }
 
 function renderHPBar() {
-    this.elHPBar.style.width = (this.HP * 100) / this.defaultHP  + '%';
-}
-
-function getDamage(count) {
-    const damageCount = Math.ceil((this.defaultHP / 100) * count);
-
-    this.HP -= damageCount;
-
-    console.log('HP ' + this.name + this.HP);
-    console.log('Earned damage: ' + Math.ceil((this.defaultHP / 100) * count));
-
-    if (count > (this.HP * 100) / this.defaultHP) {
-        this.HP = 0;
-
-        if (this.name === character.name) {
-
-            console.log(enemy.name +' make last kick ' + '(' + damageCount +') ' + 'to ' + character.name);
-        } else {
-            console.log(character.name +' make last kick ' + '(' + damageCount +') ' + 'to ' + enemy.name);
-        }
-
-        console.log('Бедный ' + this.name + ' проиграл бой!');
-        disableALlButtons(actions);
-    }
-
-    if (this.name === character.name) {
-        makeLog(character, enemy, damageCount);
-    } else {
-        makeLog(enemy, character, damageCount);
-    }
-
-
-
-    this.renderHP();
-
+    this.elHPBar.style.width = (this.HP * 100) / this.defaultHP + '%';
 }
 
 function random(num) {
     return Math.ceil(Math.random() * num);
 }
 
-function setupHitButtons(settings) {
-    for (let i = 0; i < settings.length; i++) {
-        settings[i].elButton.addEventListener('click', function () {
-            console.log('Kick');
-            character.getDamage(random(settings[i].damageMultiplier));
-            enemy.getDamage(random(settings[i].damageMultiplier));
-        });
+function increaseCounter() {
+    let actionCounter = this.counter;
+    return function () {
+        return ++actionCounter
     }
 }
 
-function makeLog(target, person, damage) {
+function makeActionLog() {
     const consoleBar = document.getElementById('logs');
     const consoleLog = document.createElement('p');
-    consoleLog.innerText = generateLog(target, person, damage);
-    consoleBar.insertBefore(consoleLog, consoleBar.children[0])
+    if (this.counter <= this.limit) {
+        consoleLog.innerText = `Колличество применений навыка "${this.name}" : ${this.counter}. Осталось: ${this.limit - this.counter}`;
+        consoleBar.insertBefore(consoleLog, consoleBar.children[0]);
+    } else {
+        consoleLog.innerText = `Больше нельзя применить этот навык`;
+        consoleBar.insertBefore(consoleLog, consoleBar.children[0]);
+    }
+
+}
+
+
+function setupHitButtons(buttons) {
+    for (let i = 0; i < buttons.length; i++) {
+
+        const actionCount = buttons[i].increaseCounter();
+
+        buttons[i].elButton.addEventListener('click', function () {
+            console.log('Kick');
+            console.log(`Counter call`);
+            buttons[i].counter = actionCount();
+
+            if (buttons[i].counter <= buttons[i].limit) {
+                character.getDamage(random(buttons[i].damageMultiplier));
+                enemy.getDamage(random(buttons[i].damageMultiplier));
+                buttons[i].makeActionLog();
+            } else {
+                buttons[i].elButton.disabled = true;
+                buttons[i].makeActionLog();
+            }
+        });
+    }
 }
 
 function disableALlButtons(buttons) {
@@ -109,7 +111,7 @@ function disableALlButtons(buttons) {
     }
 }
 
-function generateLog(firstPerson, secondPerson , damage) {
+function generateHitLog(firstPerson, secondPerson, damage) {
     const logs = [
         `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`, // -12, [88/100]
         `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`,
@@ -125,7 +127,45 @@ function generateLog(firstPerson, secondPerson , damage) {
     return logs[random(logs.length - 1)];
 }
 
-function createConsole () {
+
+function makeHItLog(target, person, damage) {
+    const consoleBar = document.getElementById('logs');
+    const consoleLog = document.createElement('p');
+    consoleLog.innerText = generateHitLog(target, person, damage);
+    consoleBar.insertBefore(consoleLog, consoleBar.children[0]);
+}
+
+function getDamage(count) {
+    const damageCount = Math.ceil((this.defaultHP / 100) * count);
+
+    this.HP -= damageCount;
+
+    console.log('HP ' + this.name + this.HP);
+    console.log('Earned damage: ' + Math.ceil((this.defaultHP / 100) * count));
+
+    if (count > (this.HP * 100) / this.defaultHP) {
+        this.HP = 0;
+
+        if (this.name === character.name) {
+            console.log(`${enemy.name} make last kick (${damageCount}) to ${character.name}`);
+        } else {
+            console.log(`${character.name} make last kick (${damageCount}) to ${enemy.name}`);
+        }
+
+        console.log('Бедный ' + this.name + ' проиграл бой!');
+        disableALlButtons(actions);
+    }
+
+    if (this.name === character.name) {
+        makeHItLog(character, enemy, damageCount);
+    } else {
+        makeHItLog(enemy, character, damageCount);
+    }
+
+    this.renderHP();
+}
+
+function createConsole() {
     const consoleBlock = document.createElement('div');
     const playGround = document.querySelector('html');
     playGround.appendChild(consoleBlock).setAttribute('id', 'logs');
